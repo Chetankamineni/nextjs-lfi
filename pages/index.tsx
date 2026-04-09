0c1ed18a8bde49a5ce0d8ba19203d89db933560d
Chetankamineni <sridhar.kamineni@outlook.com>
2026-04-09 11:21:39 +0530

    updated nextjs router from app to page

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Next.js</title>
+        <meta name="description" content="Next.js app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <main>
+        <h1>Welcome to Next.js</h1>
+      </main>
+    </>
+  )
+}
